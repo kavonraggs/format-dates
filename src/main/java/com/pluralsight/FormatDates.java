8dc42777cd1bdc0c3a26cef3e77ee01d91c34fcd
kavonraggs <kavonraggs@gmail.com>
2025-10-08 11:10:40 -0500

    date formatter local and mt time

--- src/main/java/com/pluralsight/FormatDates.java
+++ src/main/java/com/pluralsight/FormatDates.java
@@ -1,11 +1,14 @@
 package com.pluralsight;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class FormatDates {
     public static void main(String[] args) {
         LocalDateTime today = LocalDateTime.now();
+
         DateTimeFormatter fmt =
                 DateTimeFormatter.ofPattern("MM/dd/yyyy");
         String slashDate = today.format(fmt);
@@ -18,15 +21,21 @@ public class FormatDates {
                 DateTimeFormatter.ofPattern("MMM dd, yyyy");
         String writtenDate = today.format(fmt3);
 
-
+        ZonedDateTime gmtNow = ZonedDateTime.now(ZoneId.of("GMT"));
         DateTimeFormatter fmt4 =
                 DateTimeFormatter.ofPattern("E, MMM dd, yyyy HH:mm");
-        String writtenDateAndTime = today.format(fmt4);
+        String writtenDateAndTimeGMT = gmtNow.format(fmt4);
+
+        DateTimeFormatter fmt5 =
+                DateTimeFormatter.ofPattern("HH: mm 'on' dd-MMM-yyyy");
+        String writtenDateAndTimeLocal = today.format(fmt5);
+
 
         System.out.println(slashDate);
         System.out.println(dashDate);
         System.out.println(writtenDate);
-        System.out.println(writtenDateAndTime);
+        System.out.println("GMT: " + writtenDateAndTimeGMT);
+        System.out.println("Local: " + writtenDateAndTimeLocal);
 
     }
 }
